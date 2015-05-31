36ebdb01aaee9e26d641b02e8377c552d83274b5
ninianh <hungbill88@gmail.com>
2015-05-31 19:47:05 +0700

    Add License Manager

--- Projects_branches/PhongKham/PhongKham/PhongKham/PhongKham/Program.cs
+++ Projects_branches/PhongKham/PhongKham/PhongKham/PhongKham/Program.cs
@@ -9,6 +9,7 @@ using System.Xml;
 using MySql.Data.MySqlClient;
 using System.IO;
 using Clinic.Database;
+using SoftwareLocker;
 
 namespace PhongKham
 {
@@ -32,6 +33,29 @@ namespace PhongKham
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+
+
+            TrialMaker t = new TrialMaker("Clinic", Application.StartupPath + "\\RegFile.reg",
+    Environment.GetFolderPath(Environment.SpecialFolder.System) + "\\TMSetp.dbf",
+    "Phone: +98 21 88281536\nMobile: +98 912 2881860",
+    5, 10, "745");
+
+            byte[] MyOwnKey = { 97, 250, 1, 5, 84, 21, 7, 63,
+            4, 54, 87, 56, 123, 10, 3, 62,
+            7, 9, 20, 36, 37, 21, 101, 57};
+            t.TripleDESKey = MyOwnKey;
+
+            TrialMaker.RunTypes RT = t.ShowDialog();
+            bool is_trial;
+            if (RT != TrialMaker.RunTypes.Expired)
+            {
+                if (RT == TrialMaker.RunTypes.Full)
+                    is_trial = false;
+                else
+                    is_trial = true;
+
+                //Application.Run(new Form1(is_trial));
+            }     
 
 
             if (!File.Exists("WriteLines.txt"))
